81be6e60d400ddb00036502ff27592f8ceed6a2f
Anthony Nelson <67963151+daPharaoh@users.noreply.github.com>
2021-08-04 03:50:37 -0400

    Initial commit

---
+++ Globalynk Server (1).cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace globalynk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
